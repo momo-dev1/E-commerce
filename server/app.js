4e0c93cc1f67cf8c016d6344fa43e928e21eeb5d
momo-dev1 <didy1234567@gmail.com>
2023-03-07 22:25:13 +0200

    initial commit

---
+++ server/app.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+require('express-async-errors');
+
+const chalk = require('chalk')
+const express = require("express")
+const app = express();
+
+const productsRouter = require('./routes/products');
+const categoriesRouter = require('./routes/products');
+const connectDB = require('./db/connect');
+
+
+app.use(express.json());
+
+// routes
+app.use('/api/v1/products', productsRouter)
+app.use('/api/v1/categories', categoriesRouter)
+
+
+
+
+const port = process.env.DB_CONNECT || 5000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`server is listening on port ${ chalk.yellowBright(port) }`)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start();
